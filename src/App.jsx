f005f3b64f04bb24859801a1afc27d29209e3d9a
adnanertorer <adnanertorer@gmail.com>
2024-06-18 01:02:27 +0300

    statistics

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import CartPage from "./pages/CartPage";
 import HomePage from "./pages/HomePage";
 import BillPage from "./pages/BillPage";
 import CustomerPage from "./pages/CustomerPage";
+import StatisticPage from "./pages/StatisticPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/cart" element={<CartPage />} />
         <Route path="/bills" element={<BillPage />} />,
         <Route path="/customers" element={<CustomerPage />} />
+        <Route path="/statistics" element={<StatisticPage />} />
       </Routes>
       </BrowserRouter>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -46,7 +46,7 @@ const Header = () => {
             <UserOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">Müşteriler</span>
           </Link>
-          <Link to={"/"} className="menu-link flex flex-col hover:text-[#40a9ff] transition-all">
+          <Link to={"/statistics"} className="menu-link flex flex-col hover:text-[#40a9ff] transition-all">
             <BarChartOutlined className="md:text-2xl text-xl" />
             <span className="md:text-xs text-[10px]">İstatistikler</span>
           </Link>
