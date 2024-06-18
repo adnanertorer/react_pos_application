16222eb527b91c49c04ddf22480132e15d612e12
adnanertorer <adnanertorer@gmail.com>
2024-06-18 13:38:26 +0300

    added products

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,33 +1,48 @@
-const Categories = () => {
-    return (
-      <ul className="flex gap-4 md:flex-col text-lg">
-        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
-          <span>Tümü</span>
+import { PlusOutlined, EditOutlined  } from "@ant-design/icons";
+import { useState } from "react";
+import "./style.css";
+import Add from "./Add";
+import Edit from "./Edit";
+
+
+const Categories = ({categories, setCategories}) => {
+
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+
+  return (
+    <ul className="flex gap-4 md:flex-col text-lg">
+      {categories.map((item)=>(
+        <li className="category-item" key={item.id}>
+          <span>{item.title}</span>
         </li>
-        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
-          <span>Yiyecek</span>
-        </li>
-        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
-          <span>İçecek</span>
-        </li>
-        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
-          <span>İçecek</span>
-        </li>
-        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
-          <span>İçecek</span>
-        </li>
-        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
-          <span>İçecek</span>
-        </li>
-        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
-          <span>İçecek</span>
-        </li>
-        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
-          <span>İçecek</span>
-        </li>
-     
-      </ul>
-    );
-  };
-  
-  export default Categories;
+      ))}
+      <li className="category-item !bg-purple-800 hover:opacity-90"
+        onClick={() => setIsAddModalOpen(true)}
+      >
+        <PlusOutlined className="md:text-2xl" />
+      </li>
+      <li
+        className="category-item !bg-orange-800 hover:opacity-90"
+        onClick={() => setIsEditModalOpen(true)}
+      >
+      <EditOutlined className="md:text-2xl" />
+      </li>
+      <Add
+        isAddModalOpen={isAddModalOpen}
+        setIsAddModalOpen={setIsAddModalOpen}
+        categories={categories}
+        setCategories={setCategories}
+      />
+      <Edit
+        isEditModalOpen={isEditModalOpen}
+        setIsEditModalOpen={setIsEditModalOpen}
+        categories={categories}
+        setCategories={setCategories}
+      />
+    </ul>
+  );
+};
+
+export default Categories;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,17 +2,35 @@ import Header from '../components/Header';
 import Categories from '../components/categories/Categories';
 import Products from '../components/products/Products';
 import CartTotals from '../components/cart/CartTotals';
+import { useEffect, useState } from "react";
 
 const HomePage = () => {
+    const[categories, setCategories] = useState([]);
+
+   useEffect(() =>{
+    const fetchCategories = async () => {
+        try {
+            const response = await fetch("http://localhost:5000/api/categories/get-all");
+            const data = await response.json();
+            data && setCategories(data.map((item)=>{
+                return {...item, value: item.title};
+            }));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    fetchCategories();
+   });
+    
     return (
         <>
             <Header />
             <div className="home px-6 flex md:flex-row flex-col justify-between gap-10 md:pb-0 pb-24">
                 <div className="categories overflow-auto max-h-[calc(100vh_-_112px)] md:pb-10">
-                    <Categories />
+                    <Categories categories={categories} setCategories={setCategories} />
                 </div>
                 <div className="products flex-[8] max-h-[calc(100vh_-_112px)] overflow-y-auto pb-10">
-                    <Products />
+                    <Products categories={categories} />
                 </div>
                 <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
                     <CartTotals />
