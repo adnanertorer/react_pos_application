73a428646269a8a34caa1757a99221f509a1c89e
adnanertorer <adnanertorer@gmail.com>
2024-06-20 22:11:54 +0300

    added cart

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -6,7 +6,22 @@ const cartSlice = createSlice({
         cartItems: [],
         total: 0
     },
-    reducers: {}
+    reducers: {
+        addToCart: (state, action) => {
+            state.cartItems.push(action.payload);
+            state.total = state.total + action.payload.price;
+        },
+        removeFromCart: (state, action) => {
+            state.cartItems = state.cartItems.filter(item => item.id!== action.payload.id);
+            state.total = state.total - action.payload.price;
+        },
+        clearCart: (state) => {
+            state.cartItems = [];
+            state.total = 0;
+        }
+    }
 });
+
+export const { addToCart, removeFromCart, clearCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
